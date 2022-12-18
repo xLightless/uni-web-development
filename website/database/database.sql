217e7c755568557bc8ac20aa63e643fb21be4f0d
xLightless <lightlessgaming@gmail.com>
2022-12-18 05:35:09 +0000

    Refactored database and added normalisation data

--- website/database/database.sql
+++ website/database/database.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: horizon_travels
+-- Host: 127.0.0.1    Database: ht_database
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
@@ -16,114 +16,136 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `account`
+-- Table structure for table `accounts`
 --
 
-DROP TABLE IF EXISTS `account`;
+DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `account` (
-  `accountID` int NOT NULL,
-  `username` varchar(15) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
-  `isEmailActivated` int DEFAULT NULL,
-  `userType` varchar(15) DEFAULT NULL,
-  `customerID` int DEFAULT NULL,
-  PRIMARY KEY (`accountID`),
-  KEY `customerID_idx` (`customerID`),
-  CONSTRAINT `customerID` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerID`)
+CREATE TABLE `accounts` (
+  `account_id` int NOT NULL,
+  `contact_id` int DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `user_type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`account_id`),
+  KEY `contact_id_idx` (`contact_id`),
+  CONSTRAINT `contact_id` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `account`
+-- Dumping data for table `accounts`
 --
 
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `booking`
+-- Table structure for table `bookings`
 --
 
-DROP TABLE IF EXISTS `booking`;
+DROP TABLE IF EXISTS `bookings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `booking` (
-  `bookingID` int NOT NULL,
-  `telephone` int DEFAULT NULL,
-  `paymentMethod` varchar(45) DEFAULT NULL,
-  `accountID` int DEFAULT NULL,
-  `travellingID` int DEFAULT NULL,
-  `discountID` int DEFAULT NULL,
-  PRIMARY KEY (`bookingID`),
-  KEY `customerID_idx` (`accountID`),
-  KEY `travellingID_idx` (`travellingID`),
-  KEY `discountID_idx` (`discountID`),
-  CONSTRAINT `accountID` FOREIGN KEY (`accountID`) REFERENCES `account` (`accountID`),
-  CONSTRAINT `discountID` FOREIGN KEY (`discountID`) REFERENCES `discounts` (`discountID`),
-  CONSTRAINT `travellingID` FOREIGN KEY (`travellingID`) REFERENCES `travel` (`travellingID`)
+CREATE TABLE `bookings` (
+  `booking_id` int NOT NULL,
+  `booking_date` date DEFAULT NULL,
+  `journey_seat_types` varchar(8) DEFAULT NULL,
+  `advance_booking_days` int DEFAULT NULL,
+  `booking_departure_date` date DEFAULT NULL,
+  `booking_return_date` date DEFAULT NULL,
+  PRIMARY KEY (`booking_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `booking`
+-- Dumping data for table `bookings`
 --
 
-LOCK TABLES `booking` WRITE;
-/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
-/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `customer`
+-- Table structure for table `contacts`
 --
 
-DROP TABLE IF EXISTS `customer`;
+DROP TABLE IF EXISTS `contacts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `customer` (
-  `customerID` int NOT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `firstName` varchar(30) DEFAULT NULL,
-  `lastName` varchar(30) DEFAULT NULL,
-  `dateOfBirth` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`customerID`)
+CREATE TABLE `contacts` (
+  `contact_id` int NOT NULL,
+  `customer_id` int DEFAULT NULL,
+  `telephone` varchar(15) DEFAULT NULL,
+  `email_address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`contact_id`),
+  KEY `customer_id_idx` (`customer_id`),
+  CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `customer`
+-- Dumping data for table `contacts`
 --
 
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `discounts`
+-- Table structure for table `customers`
 --
 
-DROP TABLE IF EXISTS `discounts`;
+DROP TABLE IF EXISTS `customers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `discounts` (
-  `discountID` int NOT NULL,
-  `discountType` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`discountID`)
+CREATE TABLE `customers` (
+  `customer_id` int NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `discounts`
+-- Dumping data for table `customers`
 --
 
-LOCK TABLES `discounts` WRITE;
-/*!40000 ALTER TABLE `discounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `discounts` ENABLE KEYS */;
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `journey`
+--
+
+DROP TABLE IF EXISTS `journey`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `journey` (
+  `journey_id` int NOT NULL,
+  `departure` varchar(45) DEFAULT NULL,
+  `departure_time` datetime DEFAULT NULL,
+  `return` varchar(45) DEFAULT NULL,
+  `return_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`journey_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `journey`
+--
+
+LOCK TABLES `journey` WRITE;
+/*!40000 ALTER TABLE `journey` DISABLE KEYS */;
+/*!40000 ALTER TABLE `journey` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -134,14 +156,18 @@ DROP TABLE IF EXISTS `travel`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `travel` (
-  `travellingID` int NOT NULL,
-  `travelType` varchar(45) DEFAULT NULL,
-  `dateOfTravelOut` varchar(45) DEFAULT NULL,
-  `dateOfTravelReturn` varchar(45) DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `originLocation` varchar(45) DEFAULT NULL,
-  `destinationLocation` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`travellingID`)
+  `travel_id` int NOT NULL,
+  `journey_id` int DEFAULT NULL,
+  `travel_type` varchar(15) DEFAULT NULL,
+  `price` decimal(7,2) DEFAULT NULL,
+  `discount_percentage` varchar(15) DEFAULT NULL,
+  `payment_method` varchar(15) DEFAULT NULL,
+  `account_id` int DEFAULT NULL,
+  PRIMARY KEY (`travel_id`),
+  KEY `journey_id_idx` (`journey_id`),
+  KEY `account_id_idx` (`account_id`),
+  CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`),
+  CONSTRAINT `journey_id` FOREIGN KEY (`journey_id`) REFERENCES `journey` (`journey_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -153,6 +179,34 @@ LOCK TABLES `travel` WRITE;
 /*!40000 ALTER TABLE `travel` DISABLE KEYS */;
 /*!40000 ALTER TABLE `travel` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL,
+  `booking_id` int DEFAULT NULL,
+  `travel_id` int DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `booking_id_idx` (`booking_id`),
+  KEY `travel_id_idx` (`travel_id`),
+  CONSTRAINT `booking_id` FOREIGN KEY (`booking_id`) REFERENCES `bookings` (`booking_id`),
+  CONSTRAINT `travel_id` FOREIGN KEY (`travel_id`) REFERENCES `travel` (`travel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -163,4 +217,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-06 22:30:44
+-- Dump completed on 2022-12-18  5:25:19
